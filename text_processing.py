b17e38e15fae6a59423c4c00933d5f6e7608a80f
mominjafri <55677965+mominjafri@users.noreply.github.com>
2025-01-25 20:07:46 -0800

    Delete text_processing.py

--- text_processing.py
+++
@@ -1,8 +0,0 @@
-from googletrans import Translator
-
-def translate_text(text, dest_language):
-    print(text)
-    translator = Translator()
-    translated = translator.translate(text, dest=dest_language).text
-    print (translated)
-    return translated
